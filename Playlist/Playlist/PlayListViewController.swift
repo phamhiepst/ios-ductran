d43c2db2ade7cceef9755f4cc1f10a9a8a270fcc
phamhiepst <phamhiepst@gmail.com>
2016-04-24 17:49:11 +0700

    [Hiep Pham] initial

---
+++ Playlist/Playlist/PlayListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlayListViewController.swift
+//  Playlist
+//
+//  Created by Hiep Pham on 4/24/16.
+//  Copyright © 2016 phamhiepst. All rights reserved.
+//
+
+import UIKit
+
+class PlayListViewController: UIViewController {
+
+    @IBOutlet weak var coverImage0: UIImageView!
+    @IBOutlet weak var coverImage1: UIImageView!
+    @IBOutlet weak var coverImage2: UIImageView!
+    @IBOutlet weak var coverImage3: UIImageView!
+    @IBOutlet weak var coverImage4: UIImageView!
+    @IBOutlet weak var coverImage5: UIImageView!
+    @IBOutlet weak var coverImage6: UIImageView!
+    @IBOutlet weak var coverImage7: UIImageView!
+    @IBOutlet weak var coverImage8: UIImageView!
+    @IBOutlet weak var coverImage9: UIImageView!
+    @IBOutlet weak var coverImage10: UIImageView!
+    @IBOutlet weak var coverImage11: UIImageView!
+    
+    var coverImages: [UIImageView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // append all the cover images into the coverImages array
+        coverImages = [coverImage0,coverImage1,coverImage2,coverImage3,coverImage4,coverImage5,coverImage6,coverImage7,coverImage8,coverImage9,coverImage10,coverImage11]
+        
+        updateUI()
+    }
+    
+    // set the cover images for those UIOulets
+    func  updateUI(){
+        for coverImage in coverImages{
+            coverImage.image = UIImage(named: "A Hard Day's Night")
+        }
+    }
+}
